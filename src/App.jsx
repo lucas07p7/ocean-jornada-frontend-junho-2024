d928b246150e38d43d68b305c830f1a0e709d9b0
Lucas Cavalcante <104601028+lucas07p7@users.noreply.github.com>
2024-06-06 14:29:43 +0000

    Estrutura html criada

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ function App() {
 
   return (
     <>
-      App
+      <div>
+        <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
+      </div>
       </>
   )
 }
