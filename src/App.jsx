b55f1dd9c4e485f14861e6434edfaf12b6123fb5
Lucas Cavalcante <104601028+lucas07p7@users.noreply.github.com>
2024-06-07 14:31:28 +0000

    Novo card

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,17 @@ function App() {
     image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
   }
 
+  const item3 = {
+    name: 'Summer Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+
   return (
     <>
     <div className="cards">
     <Card item={item1} /> 
     <Card item={item2} />
+    <Card item={item3} />
     </div> 
       </>
   )
