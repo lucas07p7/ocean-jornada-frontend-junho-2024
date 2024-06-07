3630bc77bb5b73081141fac2388e6377bb7ca4ec
Lucas Cavalcante <104601028+lucas07p7@users.noreply.github.com>
2024-06-07 12:56:46 +0000

    Card editado

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,18 @@ function App() {
   return (
     <>
       <div className= "card">
-        <h2>Rick Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
+        <h2 className= "nome"> Rick Sanchez</h2>
+        
+        <div className="tag">
+          <div>Status: Vivo</div>
+          <div>Espécie: Humana</div>
+          <div>Origem: Terra C-137</div>
+        </div>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez" />
       </div>
       </>
   )
-}
 
+}
 export default App
+
