e8e57fd792972e2031fbb56c881de9b7fe9e6a20
Lucas Cavalcante <104601028+lucas07p7@users.noreply.github.com>
2024-06-07 13:27:39 +0000

    Estilização dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
 
   return (
     <>
-      <div className= "card">
-        <h2 className= "nome"> Rick Sanchez</h2>
-        
-        <div className="tag">
-          <div>Status: Vivo</div>
-          <div>Espécie: Humana</div>
-          <div>Origem: Terra C-137</div>
-        </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez" />
-      </div>
+    <div className="cards">
+    <Card/> 
+    <Card/>
+    </div> 
       </>
   )
 
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card (){
+    return (     <div className= "card">
+    <h2 className= "nome"> Rick Sanchez</h2>
+    
+    <div className="tags">
+      <div>Status: Vivo</div>
+      <div>Espécie: Humana</div>
+      <div>Origem: Terra C-137</div>
+    </div>
+    <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez" />
+  </div>
+    )
+}
