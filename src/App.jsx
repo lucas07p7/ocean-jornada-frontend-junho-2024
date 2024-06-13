fbaedcb1e9fa62c306469e8c76be326ff41920c6
Lucas Cavalcante <104601028+lucas07p7@users.noreply.github.com>
2024-06-13 12:56:22 +0000

    Implementando Api

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,39 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  const item1 = {
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  // const item1 = {
+  //   name: 'Rick Sanchez',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  // }
+
+  // const item2 = {
+  //   name: 'Morty Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  // }
+
+  // const item3 = {
+  //   name: 'Summer Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  // }
+
+  // //Lista (arrays)
+  // const itens = [item1, item2, item3]
+
+  const itens = []
+
+  async function carregarDados(){
+
+  const apiUrl = 'https://rickandmortyapi.com/api/character'
+
+  const response = await fetch(apiUrl)
+
+  const body = await response.json()
+
+  console.log(body)
   }
 
-  const item2 = {
-    name: 'Morty Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-  }
+  carregarDados()
 
-  const item3 = {
-    name: 'Summer Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  }
-
-  //Lista (arrays)
-  const itens = [item1, item2, item3]
 
   return (
     <>
